f3bc8282dd63b9bba79432f14578b0aab854554e
acacopardo <pookiie57@gmail.com>
2017-03-12 11:51:37 +0100

    Add anv

--- event.php
+++ event.php
@@ -14,7 +14,7 @@
 
 </head>
 <body>
-
+<?php include_once 'header.php'; ?>
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -23,76 +23,28 @@
 
                     <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                         <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-                    </button> <a class="navbar-brand" href="#">Brand</a>
+                    </button> <a class="navbar-brand" href="http://flashevents.flash-global.net/map.php">Flash Events</a>
                 </div>
 
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
                         <li class="active">
-                            <a href="#">Link</a>
+                            <a href="http://flashevents.flash-global.net/event.html">Event</a>
                         </li>
                         <li>
-                            <a href="#">Link</a>
+                            <a href="http://flashevents.flash-global.net/map.php">Map</a>
                         </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Something else here</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">Separated link</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">One more separated link</a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                    <form class="navbar-form navbar-left" role="search">
-                        <div class="form-group">
-                            <input type="text" class="form-control">
-                        </div>
-                        <button type="submit" class="btn btn-default">
-                            Submit
-                        </button>
-                    </form>
-                    <ul class="nav navbar-nav navbar-right">
                         <li>
-                            <a href="#">Link</a>
+                            <a href="http://flashevents.flash-global.net/user.html">User</a>
                         </li>
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown<strong class="caret"></strong></a>
-                            <ul class="dropdown-menu">
-                                <li>
-                                    <a href="#">Action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Another action</a>
-                                </li>
-                                <li>
-                                    <a href="#">Something else here</a>
-                                </li>
-                                <li class="divider">
-                                </li>
-                                <li>
-                                    <a href="#">Separated link</a>
-                                </li>
-                            </ul>
+                        <li>
+                            <a href="http://flashevents.flash-global.net/friends.html">Freinds</a>
+                        </li>
+                        <li>
+                            <a href="http://flashevents.flash-global.net/transport.html">Transport</a>
                         </li>
                     </ul>
                 </div>
-
             </nav>
         </div>
     </div>
