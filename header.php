15cca91a198330a8c36a7e8e1362befce77d6ceb
acacopardo <pookiie57@gmail.com>
2017-03-12 11:56:53 +0100

    Upadte href

--- header.php
+++ header.php
@@ -9,9 +9,9 @@
 </div>
 <input id="menuToggle" class="hidden" type="checkbox" />
 <ul id="menu">
-    <li><a href="http://flashevents.flash-global.net/event.html">Event</a></li>
+    <li><a href="http://flashevents.flash-global.net/event.php">Event</a></li>
     <li><a href="http://flashevents.flash-global.net/map.php">Map</a></li>
-    <li><a href="http://flashevents.flash-global.net/user.html">User</a></li>
-    <li><a href="http://flashevents.flash-global.net/friends.html">Friends</a></li>
-    <li><a href="http://flashevents.flash-global.net/transport.html">Transport</a></li>
+    <li><a href="http://flashevents.flash-global.net/user.php">User</a></li>
+    <li><a href="http://flashevents.flash-global.net/friends.php">Friends</a></li>
+    <li><a href="http://flashevents.flash-global.net/transport.php">Transport</a></li>
 </ul>
